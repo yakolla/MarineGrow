04dee71889f8831f57352d1e168a35475cfb248e
yakolla <yakolla@gmail.com>
2014-10-17 23:03:39 +0900

    파이어건 조정

--- Assets/Script/Weapon.cs
+++ Assets/Script/Weapon.cs
@@ -4,11 +4,22 @@ using System.Collections;
 public class Weapon : MonoBehaviour {
 
 	protected GameObject	m_aimpoint = null;
-
+	bool					m_firing = false;
 
 	virtual public void Init(GameObject aimpoint)
 	{
 		m_aimpoint = aimpoint;
+		m_firing = true;
+	}
+
+	virtual public void StopFiring()
+	{
+		m_firing = false;
+	}
+
+	public bool IsFiring()
+	{
+		return m_firing;
 	}
 
 }
