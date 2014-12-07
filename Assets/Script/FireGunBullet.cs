8d406afe4d27bf30935cb57cf64bd0a77244c591
yakolla <yakolla@gmail.com>
2014-12-07 12:14:43 +0900

    파이건은 관통되지 않도록 수정

--- Assets/Script/FireGunBullet.cs
+++ Assets/Script/FireGunBullet.cs
@@ -7,10 +7,15 @@ public class FireGunBullet : Bullet {
 
 	float			m_lastDamageTime = 0f;
 	BoxCollider		m_collider;
+
+	ParticleSystem	m_particleSystem;
+
 	// Use this for initialization
 	void Start () 
 	{
 		m_collider = GetComponent<BoxCollider>();
+		m_particleSystem = transform.Find("Body/Particle System").particleSystem;
+
 	}
 	
 	// Update is called once per frame
@@ -19,11 +24,36 @@ public class FireGunBullet : Bullet {
 		if (m_lastDamageTime+m_damageOnTime<Time.time)
 		{
 			m_collider.enabled = true;
+
 			m_lastDamageTime = Time.time;
 		}
 		else
 		{
 			m_collider.enabled = false;
+		}
+
+		RaycastHit hit;
+		Vector3 fwd = transform.TransformDirection(Vector3.right);
+		if (Physics.Raycast(transform.position, fwd, out hit, 10f))
+		{
+			Creature creature = hit.transform.gameObject.GetComponent<Creature>();
+			if (creature && Creature.IsEnemy(creature, m_ownerCreature))
+			{				
+				float dist = Mathf.Min(Vector3.Distance(m_ownerCreature.transform.position, creature.transform.position)+2, 8f);
+				m_particleSystem.startSpeed = dist;
+
+				m_collider.center = new Vector3(dist/2.6f, m_collider.center.y, m_collider.center.z);
+				m_collider.size = new Vector3(dist, m_collider.size.y, m_collider.size.z);
+
+				if (m_collider.enabled)
+				{
+					creature.TakeDamage(m_ownerCreature, new DamageDesc(m_ownerCreature.m_creatureProperty.PhysicalAttackDamage, DamageDesc.Type.Fire, PrefDamageEffect));	
+				}
+
+			}
+		}
+		else{
+			m_particleSystem.startSpeed = 8f;
 		}
 	}
 
@@ -47,9 +77,15 @@ public class FireGunBullet : Bullet {
 
 	void OnTriggerEnter(Collider other) 
 	{
+		return;
 		Creature creature = other.gameObject.GetComponent<Creature>();
 		if (creature && Creature.IsEnemy(creature, m_ownerCreature))
 		{
+			float dist = Vector3.Distance(m_ownerCreature.transform.position, creature.transform.position);
+			m_particleSystem.startSpeed = dist;
+
+			m_collider.center = new Vector3(dist/2.6f, m_collider.center.y, m_collider.center.z);
+			m_collider.size = new Vector3(dist, m_collider.size.y, m_collider.size.z);
 			creature.TakeDamage(m_ownerCreature, new DamageDesc(m_ownerCreature.m_creatureProperty.PhysicalAttackDamage, DamageDesc.Type.Fire, PrefDamageEffect));			
 		}
 	}
