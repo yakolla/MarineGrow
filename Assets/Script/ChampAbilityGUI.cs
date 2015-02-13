ae8048a0bc893253da2d684596243116abfcb432
yakolla <yakolla@gmail.com>
2015-02-14 01:52:33 +0900

    고스트 슬라임 적용, 레벨업시, 마스터리 선택할 수 있도록 변경

--- Assets/Script/ChampAbilityGUI.cs
+++ Assets/Script/ChampAbilityGUI.cs
@@ -6,12 +6,14 @@ public class ChampAbilityGUI : MonoBehaviour {
 	Champ		m_champ;
 	Rect 		m_statusWindowRect;
 	Rect 		m_skillWindowRect;
-
+	const 	int MasteryTypes = 3;
+	const	int	MasteryColumns = 4;
 	[SerializeField]
 	GUISkin		m_guiSkin = null;
 
 	float 		m_width = Screen.width * (1/5f);
 	float 		m_height = Screen.height * (1/8f);
+	int		m_fontSize = (int)(Screen.width*(1/50f));
 
 	void Start () {
 
@@ -39,6 +41,48 @@ public class ChampAbilityGUI : MonoBehaviour {
 	{
 		TimeEffector.Instance.StopTime();
 	}
+
+	Vector2[] masterySrollPosition = new Vector2[MasteryTypes];
+	delegate void OnChampStat();
+	void displayMastery(Rect rect, Texture2D icon, float statPoint, ref Vector2 scrollPosition, OnChampStat onChampStat)
+	{
+		int size = (int)rect.width/MasteryColumns;
+		GUI.BeginGroup(rect);
+		GUI.Label(new Rect(0, 0, size, size), icon);
+		GUI.Label(new Rect(size, 0, size, size), statPoint.ToString());
+		if (GUI.Button(new Rect(size*3, 0, size, size), "+") && m_champ.RemainStatPoint > 0)
+		{
+			onChampStat();
+		}
+
+		int padding = size/MasteryColumns;
+
+		switch (Application.platform)
+		{
+		case RuntimePlatform.WindowsEditor:
+		case RuntimePlatform.WindowsPlayer:
+			scrollPosition = GUI.BeginScrollView(new Rect(0, size+padding, size*MasteryColumns, size*4), scrollPosition, new Rect(0, size, size*MasteryColumns-padding*2, size*8));
+			break;
+		default:
+			scrollPosition = GUI.BeginScrollView(new Rect(0, size+padding, size*MasteryColumns, size*4), scrollPosition, new Rect(0, size+padding, size*MasteryColumns-padding*2, size*8),GUIStyle.none,GUIStyle.none);
+			break;
+		}
+
+		GUIStyle itemCountStyle = m_guiSkin.GetStyle("ItemCount");
+		itemCountStyle.fontSize = m_fontSize;
+
+		for(int y = 0; y < 6; ++y)
+		{
+			for(int x = 0; x < (MasteryColumns-1); ++x)
+			{
+				GUI.Button(new Rect(padding+size*x+padding*x, size+padding+y*size+padding*y, size, size), icon);
+				GUI.Label(new Rect(padding+size*x+padding*x, size+padding+y*size+padding*y, size, size), "<color=white>0/0</color>", itemCountStyle);
+			}
+		}
+
+		GUI.EndScrollView();
+		GUI.EndGroup();
+	}
 	
 	//Setting up the Inventory window
 	void DisplayStatusWindow(int windowID)
@@ -46,7 +90,7 @@ public class ChampAbilityGUI : MonoBehaviour {
 		int startY = 0;
 		int size = (int)m_height;
 
-		if (GUI.Button(new Rect(Screen.width-size, 0, size, size), "X"))
+		if (GUI.Button(new Rect(Screen.width/2-size/2, Screen.height-size, size, size), "OK"))
 		{
 			this.gameObject.SetActive(false);
 			return;
@@ -55,30 +99,37 @@ public class ChampAbilityGUI : MonoBehaviour {
 		GUI.Label(new Rect(0, startY+(size*0), size, size), Resources.Load<Texture>("Sprites/level"));
 		GUI.Label(new Rect(size, startY+(size*0), size, size), m_champ.RemainStatPoint.ToString());
 
+		int masteryWidth = Screen.width/3;
+		displayMastery(new Rect(0, size, masteryWidth, Screen.height), 
+		               Resources.Load<Texture2D>("Sprites/swordoftruth"), 
+		               m_champ.m_creatureProperty.PhysicalAttackDamage, 
+		               ref masterySrollPosition[0],
+		               ()=>{
+							m_champ.m_creatureProperty.AlphaPhysicalAttackDamage+=1;
+							--m_champ.RemainStatPoint;
+						}
+		);
 
-		GUI.Label(new Rect(0, startY+(size*1), size, size), Resources.Load<Texture>("Sprites/swordoftruth"));
-		GUI.Label(new Rect(size, startY+(size*1), size, size), m_champ.m_creatureProperty.PhysicalAttackDamage.ToString());
-		if (GUI.Button(new Rect(size+size, startY+(size*1), size, size), "+") && m_champ.RemainStatPoint > 0)
-		{
-			m_champ.m_creatureProperty.AlphaPhysicalAttackDamage+=1;
-			--m_champ.RemainStatPoint;
-		}
+		displayMastery(new Rect(masteryWidth, size, masteryWidth, Screen.height), 
+		               Resources.Load<Texture2D>("Sprites/staffoflight"), 
+		               m_champ.m_creatureProperty.PhysicalDefencePoint, 
+		               ref masterySrollPosition[1],
+		               ()=>{
+							m_champ.m_creatureProperty.AlphaPhysicalDefencePoint+=1;
+							--m_champ.RemainStatPoint;
+						}
+		);
 
-		GUI.Label(new Rect(0, startY+(size*2), size, size), Resources.Load<Texture>("Sprites/staffoflight"));
-		GUI.Label(new Rect(size, startY+(size*2), size, size), m_champ.m_creatureProperty.PhysicalDefencePoint.ToString());
-		if (GUI.Button(new Rect(size+size, startY+(size*2), size, size), "+") && m_champ.RemainStatPoint > 0)
-		{
-			m_champ.m_creatureProperty.AlphaPhysicalDefencePoint+=1;
-			--m_champ.RemainStatPoint;
-		}
+		displayMastery(new Rect(masteryWidth*2, size, masteryWidth, Screen.height), 
+		               Resources.Load<Texture2D>("Sprites/robeofpower"), 
+		               m_champ.m_creatureProperty.MaxHP, 
+		               ref masterySrollPosition[2],
+		               ()=>{
+							m_champ.m_creatureProperty.AlphaMaxHP+=1;
+							--m_champ.RemainStatPoint;
+						}
+		);
 
-		GUI.Label(new Rect(0, startY+(size*3), size, size), Resources.Load<Texture>("Sprites/robeofpower"));
-		GUI.Label(new Rect(size, startY+(size*3), size, size), m_champ.m_creatureProperty.AlphaMaxHP.ToString());
-		if (GUI.Button(new Rect(size+size, startY+(size*3), size, size), "+") && m_champ.RemainStatPoint > 0)
-		{
-			m_champ.m_creatureProperty.AlphaMaxHP+=1;
-			--m_champ.RemainStatPoint;
-		}
 
 	}
 	void drawGuage(Rect size, float ratio, string lable, Texture guage)
