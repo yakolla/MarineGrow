0ef3e641254c91e788b839787bd7acad34fb717b
yakolla <yakolla@gmail.com>
2015-05-07 23:01:35 +0900

    health bar ui 새로 적용, ui 핀트 안맞는 것 수정

--- Assets/Script/FloatingGuageBarGUI.cs
+++ Assets/Script/FloatingGuageBarGUI.cs
@@ -3,41 +3,24 @@ using System.Collections;
 
 public class FloatingGuageBarGUI : MonoBehaviour {
 
-	Vector3 targetPos;
 	protected Creature	m_creature;
 
-	[SerializeField]
-	Texture		m_gague = null;
 
-	[SerializeField]
-	Texture		m_gagueEdge = null;
+	YGUISystem.GUIGuage m_guage;
 
 	// Use this for initialization
 	void Start () {
 		m_creature = transform.parent.gameObject.GetComponent<Creature>();
-
+		m_guage = new YGUISystem.GUIGuage(transform.Find("Canvas/HP").gameObject, 
+		                                   ()=>{return guageRemainRatio();}, 
+		()=>{return ""; }
+		);
 	}
 
 	// Update is called once per frame
 	void Update () {
-
-		targetPos = m_creature.transform.position;
-	}
-
-	void OnGUI()
-	{
-
-		Vector2 pos = Camera.main.WorldToViewportPoint(targetPos);
-		float startX = pos.x*Screen.width-Screen.width/25;
-		float startY = (1-pos.y)*Screen.height-Screen.height/transform.transform.localPosition.y;
-		int size = Screen.width/120;
-		int maxSize = size*10;
-
-		float ratio = guageRemainRatio();
-
-		GUI.DrawTextureWithTexCoords(new Rect(startX, startY, (maxSize-size)*ratio, size), m_gague, new Rect(0f, 0f, ratio, 1f));
-		GUI.DrawTextureWithTexCoords(new Rect(startX, startY, (maxSize-size), size), m_gagueEdge, new Rect(0f, 0f, 1f, 1f));
-
+		transform.rotation = Quaternion.Euler(Vector3.zero);
+		m_guage.Update();
 	}
 
 	virtual protected float guageRemainRatio()
