357b3d52a5844ec79d80bca0435db6744ee733b4
yakolla <yakolla@gmail.com>
2015-05-09 09:56:23 +0900

    in app 적용 중

---
+++ Assets/Script/OpenIABTest.cs
@@ -0,0 +1,118 @@
+﻿/*******************************************************************************
+ * Copyright 2012-2014 One Platform Foundation
+ *
+ *       Licensed under the Apache License, Version 2.0 (the "License");
+ *       you may not use this file except in compliance with the License.
+ *       You may obtain a copy of the License at
+ *
+ *           http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *       Unless required by applicable law or agreed to in writing, software
+ *       distributed under the License is distributed on an "AS IS" BASIS,
+ *       WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *       See the License for the specific language governing permissions and
+ *       limitations under the License.
+ ******************************************************************************/
+
+using UnityEngine;
+using OnePF;
+using System.Collections.Generic;
+
+/**
+ * Example of OpenIAB usage
+ */ 
+public class OpenIABTest : MonoBehaviour
+{
+    const string SKU = "GEM 1000";
+
+    string _label = "";
+    bool _isInitialized = false;
+    Inventory _inventory = null;
+    
+
+    private void Start()
+    {
+		// Listen to all events for illustration purposes
+		OpenIABEventManager.billingSupportedEvent += billingSupportedEvent;
+		OpenIABEventManager.billingNotSupportedEvent += billingNotSupportedEvent;
+		OpenIABEventManager.queryInventorySucceededEvent += queryInventorySucceededEvent;
+		OpenIABEventManager.queryInventoryFailedEvent += queryInventoryFailedEvent;
+		OpenIABEventManager.purchaseSucceededEvent += purchaseSucceededEvent;
+		OpenIABEventManager.purchaseFailedEvent += purchaseFailedEvent;
+		OpenIABEventManager.consumePurchaseSucceededEvent += consumePurchaseSucceededEvent;
+		OpenIABEventManager.consumePurchaseFailedEvent += consumePurchaseFailedEvent;
+
+        Init();
+    }
+
+	void Init()
+	{
+		OpenIAB.mapSku(SKU, OpenIAB_Android.STORE_GOOGLE, "gem.1000");        
+
+		var googlePublicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEApi71KrlP+P+IWL1HtgDigZ5VUd9KdvSasR/Q2ONnVSmtsGrE0abo11IXodJHeDQWfGn2KCHC1qrjUW0lX8dK/2syDFmjnvF4jHXjyAl7NZqQZzlu68XI/nBF9csCJ7eRtPG5VOdmY4LDe3skx3Re0mjDi1wnHmc5gtz8Tisa6krDNq3V0lqW9rLD1aAA/TWtXcfFQOdZVrdrFsBzizJIbz9vqBAYmh8PedBcufYH/ToRUaokdNKgjh9l+2L2zNbYi1MIQC1rUv52MKzCgJv3BUEF6pfd+2iBXSfMcDzElz8VqXRUtRcdexcTYBX29cpiNTfXznW4y+StTd2TLvbiowIDAQAB";
+
+		var options = new Options();
+		options.checkInventoryTimeoutMs = Options.INVENTORY_CHECK_TIMEOUT_MS * 2;
+		options.discoveryTimeoutMs = Options.DISCOVER_TIMEOUT_MS * 2;
+		options.checkInventory = false;
+		options.verifyMode = OptionsVerifyMode.VERIFY_ONLY_KNOWN;
+		options.prefferedStoreNames = new string[] { OpenIAB_Android.STORE_GOOGLE };
+		options.availableStoreNames = new string[] { OpenIAB_Android.STORE_GOOGLE };
+		options.storeKeys = new Dictionary<string, string> { {OpenIAB_Android.STORE_GOOGLE, googlePublicKey} };
+		options.storeSearchStrategy = SearchStrategy.INSTALLER_THEN_BEST_FIT;
+		
+		// Transmit options and start the service
+		OpenIAB.init(options);
+	}
+
+
+    private void billingSupportedEvent()
+    {
+        _isInitialized = true;
+        Debug.Log("billingSupportedEvent");
+
+		OpenIAB.purchaseProduct(SKU, "ok marine");
+    }
+    private void billingNotSupportedEvent(string error)
+    {
+        Debug.Log("billingNotSupportedEvent: " + error);
+    }
+    private void queryInventorySucceededEvent(Inventory inventory)
+    {
+        Debug.Log("queryInventorySucceededEvent: " + inventory);
+        if (inventory != null)
+        {
+            _label = inventory.ToString();
+            _inventory = inventory;
+
+
+        }
+    }
+    private void queryInventoryFailedEvent(string error)
+    {
+        Debug.Log("queryInventoryFailedEvent: " + error);
+        _label = error;
+    }
+    private void purchaseSucceededEvent(Purchase purchase)
+    {
+        Debug.Log("purchaseSucceededEvent: " + purchase);
+        _label = "PURCHASED:" + purchase.ToString();
+
+		OpenIAB.consumeProduct(purchase);
+    }
+    private void purchaseFailedEvent(int errorCode, string errorMessage)
+    {
+        Debug.Log("purchaseFailedEvent: " + errorMessage);
+        _label = "Purchase Failed: " + errorMessage;
+    }
+    private void consumePurchaseSucceededEvent(Purchase purchase)
+    {
+        Debug.Log("consumePurchaseSucceededEvent: " + purchase);
+        _label = "CONSUMED: " + purchase.ToString();
+    }
+    private void consumePurchaseFailedEvent(string error)
+    {
+        Debug.Log("consumePurchaseFailedEvent: " + error);
+        _label = "Consume Failed: " + error;
+    }
+}
